f6237a57f1da4db2d2b541e55536eb7383a0a63c
xrshenge1 <xin-ran.shen@u-psud.fr>
2020-02-23 22:17:12 +0100

    numba_CPU

--- of/main.py
+++ of/main.py
@@ -3,10 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import operators as op
 import flowpy
+from numba import jit
+import time
 image1 = np.asarray(Image.open("data/RubberWhale/frame10.png"))
 image2 = np.asarray(Image.open("data/RubberWhale/frame11.png"))
+start = time.time()
 image_dx,image_dy = op.flot_optique(image1,image2,17)
 flowpy.show_flow(image_dx,image_dy)
+end = time.time()
+print("Elapsed (with compilation) = %s" % (end - start))
 
 # image_dx = op.derivee_x(image)
 # image_dy = op.derivee_y(image)
--- of/operators.py
+++ of/operators.py
@@ -1,21 +1,37 @@
 import numpy as np
+from numba import jit
 
-
+@jit(nopython=True)
 def derivee_x(image):
 
     # Retourne la derivée en x pour tous les pixels de l'image
     hauteur, largeur = image.shape
     tab_dx = np.zeros((hauteur,largeur))
-    tab_dx[:,-1] = 0
-    tab_dx[:,0:-1] =  image[:,1:] - image[:,0:-1]    
+    for i in range (hauteur):
+        for j in range(largeur):
+            if(j == largeur-1):
+                tab_dx[i][j] = 0
+            else:
+                tab_dx[i][j] = image[i][j+1] - image[i][j]
+
+    # tab_dx[:,-1] = 0
+    # tab_dx[:,0:-1] =  image[:,1:] - image[:,0:-1]    
     return tab_dx
 
+@jit(nopython=True)
 def derivee_y(image):
     # Retourne la derivée en y pour tous les pixels de l'image
     hauteur, largeur = image.shape
     tab_dy = np.zeros((hauteur,largeur))
-    tab_dy[-1,:] = 0
-    tab_dy[0:-1,:] =  image[1:,:] - image[0:-1,:]   
+    for i in range (hauteur):
+        for j in range(largeur):
+            if(i == hauteur-1):
+                tab_dy[i][j] = 0
+            else:
+                tab_dy[i][j] = image[i+1][j] - image[i][j]
+
+    # tab_dy[-1,:] = 0
+    # tab_dy[0:-1,:] =  image[1:,:] - image[0:-1,:]   
     return tab_dy
 
 def derivee_t(image_1, image_2):
@@ -24,26 +40,28 @@ def derivee_t(image_1, image_2):
     tab_dt = np.zeros((hauteur,largeur))
     tab_dt  =  image_2 - image_1
     return tab_dt
- 
-def somme_fenetre(image,x,y,r):  # x correspond à ligne, y correspond à colone
-    hauteur,largeur = image.shape
-    #création de fenêtre
-    tab_fenetre = np.zeros((2*r+1,2*r+1))
-    nouvelle_image = np.zeros((hauteur + 2*r, largeur + 2*r))
-    nouvelle_image[r:hauteur+r,r:largeur+r] = image #milieu
-    nouvelle_image[0:r,r:largeur+r] = image[0,:]    #haut
-    nouvelle_image[hauteur+r:,r:r+largeur] = image[hauteur-1,:]  #bas
-    nouvelle_image[0:r,0:r] = image[0,0] #coin gauche_haut
-    nouvelle_image[r+hauteur:,0:r] = image[hauteur-1,0] # coin gauche_bas
-    nouvelle_image[0:r,r+largeur:] = image[0,largeur-1]  #coin droit_haut
-    nouvelle_image[r+hauteur:,r+largeur:] = image[hauteur-1,largeur-1] #coin droit_bas
-    nouvelle_image[r:r+hauteur,0:r]  = np.repeat(image[0:hauteur,0],r).reshape(hauteur,r)
-    nouvelle_image[r:r+hauteur,r+largeur:] = np.repeat(image[0:hauteur,largeur-1],r).reshape(hauteur,r)
-    #calculer la somme de tableau
-    tab_fenetre = nouvelle_image[x:x+2*r+1,y:y+2*r+1]
-    som = np.sum(tab_fenetre)
-    return som
 
+
+# def somme_fenetre(image,x,y,r):  # x correspond à ligne, y correspond à colone
+#     hauteur,largeur = image.shape
+#     #création de fenêtre
+#     tab_fenetre = np.zeros((2*r+1,2*r+1))
+#     nouvelle_image = np.zeros((hauteur + 2*r, largeur + 2*r))
+#     nouvelle_image[r:hauteur+r,r:largeur+r] = image #milieu
+#     nouvelle_image[0:r,r:largeur+r] = image[0,:]    #haut
+#     nouvelle_image[hauteur+r:,r:r+largeur] = image[hauteur-1,:]  #bas
+#     nouvelle_image[0:r,0:r] = image[0,0] #coin gauche_haut
+#     nouvelle_image[r+hauteur:,0:r] = image[hauteur-1,0] # coin gauche_bas
+#     nouvelle_image[0:r,r+largeur:] = image[0,largeur-1]  #coin droit_haut
+#     nouvelle_image[r+hauteur:,r+largeur:] = image[hauteur-1,largeur-1] #coin droit_bas
+#     nouvelle_image[r:r+hauteur,0:r]  = np.repeat(image[0:hauteur,0],r).reshape(hauteur,r)
+#     nouvelle_image[r:r+hauteur,r+largeur:] = np.repeat(image[0:hauteur,largeur-1],r).reshape(hauteur,r)
+#     #calculer la somme de tableau
+#     tab_fenetre = nouvelle_image[x:x+2*r+1,y:y+2*r+1]
+#     som = np.sum(tab_fenetre)
+#     return som
+
+@jit(nopython=True) 
 def somme_fenetre_global(image,r):
     hauteur,largeur = image.shape
     #création de fenêtre
@@ -65,6 +83,7 @@ def somme_fenetre_global(image,r):
             som_tab[i][j] = np.sum(tab_fenetre)
     return som_tab
 
+
 def flot_optique(image1,image2,rayon):
     #création des tableau
     somme_tab_dx_2 = np.zeros_like(image1)
@@ -83,7 +102,7 @@ def flot_optique(image1,image2,rayon):
     tab_dx_dy = tab_dx * tab_dy
     somme_tab_dx_dy = somme_fenetre_global(tab_dx_dy,rayon)
     #calculer somme_tab_dx_dt
-    tab_dt =  derivee_t(image1,image2)
+    tab_dt =  -derivee_t(image1,image2)
     tab_dx_dt = tab_dx * tab_dt
     somme_tab_dx_dt = somme_fenetre_global(tab_dx_dt,rayon)
     #calculer somme_tab_dy_dt
